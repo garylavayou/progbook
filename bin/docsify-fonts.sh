9d8ac622ac2be31458632411b7601fcd4e0cf372
Gary Wang <garylavayou@outlook.com>
2024-05-08 18:46:20 +0800

    fix: copy css not contain font css urls.

--- bin/docsify-fonts.sh
+++ bin/docsify-fonts.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # get necessary fonts data required by css files.
 
 THEME_DIR=${THEME_DIR:-'docs/docsify-themes'}
 DOCSIFY_DIR=${DOCSIFY_DIR:-'docs/docsify-plugins/node_modules/docsify'}
 TARGET_DIR=${TARGET_DIR:-'docs'}
-set -e
 
 if [ ! -e "$DOCSIFY_DIR" ]; then
     cwd=$(pwd)
+    # shellcheck disable=SC2046
     cd $(dirname $(dirname "$DOCSIFY_DIR"))
     npm install
     cd "$cwd"
@@ -20,16 +20,22 @@ for css in "$THEME_DIR"/*.css; do
     echo "$css"
     url=$(sed -En 's/@import\s*url\("(.*)"\);/\1/p' <"$css")
     if [ -z "$url" ]; then
-        continue
+        if [[ $(basename -s .css "$css") =~ pure ]]; then
+           /bin/cp -vf "$css" "${THEME_DIR}/fonts/$(basename $css)" 
+        fi
+    else
+        css=$(basename "$css")
+        # css_name=$(sed -E 's/(.*).css/\1/' <<< "$css")
+        wget -v \
+             --output-document="${THEME_DIR}/fonts/$css" \
+             "$url"
     fi
-    css=$(basename "$css")
-    # css_name=$(sed -E 's/(.*).css/\1/' <<< "$css")
-    wget --output-document="${THEME_DIR}/fonts/$css" "$url"
 done
 
 for css in "$THEME_DIR"/fonts/*.css; do
     echo "$css"
-    fonts=($(grep -oE 'url(\((.*)\))\s' < "$css" | sed -E 's/url\((.*)\)/\1/'))
+    font_info=$(grep -oE 'url(\((.*)\))\s' < "${css}" | sed -E 's/url\((.*)\)/\1/')
+    readarray -t fonts <<< "$font_info"
     for url in "${fonts[@]}"; do
         echo "$url"
         fontpath=$(sed -E 's/https?:\/\/(.*\.com)\/(.+)/\2/' <<<"$url")
